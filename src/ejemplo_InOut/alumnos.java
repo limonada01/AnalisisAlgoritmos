543385772695f69d18ed6cdf095c6297e3c4080b
limon <daniel.campos@est.fi.uncoma.edu.ar>
2021-09-02 17:22:51 -0300

    02_09_21

---
+++ src/ejemplo_InOut/alumnos.java
@@ -0,0 +1,100 @@
+package ejemplo_InOut;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class alumnos {
+    final static int cantMaterias = 5;
+    final static int cantEstudiantes = 20;
+    final static String notas = "src/ejemplo_InOut/notas.txt";
+    final static String mediaPorAlumno = "src/ejemplo_InOut/mediaPorAlumno.txt";
+    final static String mediaPorAsignatura = "src/ejemplo_InOut/mediaPorAsignatura.txt";
+    final static String mediaDeLaClase = "src/ejemplo_InOut/mediaDeLaClase.txt";
+    public static void calcularMediaPorAlumno() throws IOException {
+        BufferedReader buffR = new BufferedReader(new FileReader(notas));
+        BufferedWriter buffW = new BufferedWriter(new FileWriter(mediaPorAlumno));
+        Scanner sc = new Scanner(buffR);
+
+        int sumatoriaParcial = 0;
+
+        for (int i = 0; i < cantEstudiantes; i++) {
+
+            for (int j = 0; j < cantMaterias; j++) {
+                sumatoriaParcial += sc.nextInt();
+                //System.out.println(sumatoriaParcial);
+            }
+            int promedio = sumatoriaParcial / cantMaterias;
+            buffW.write(promedio + "\n");
+            sumatoriaParcial = 0;
+
+        }
+        buffW.close();
+        buffR.close();
+    }
+
+    public static void calcularMediaPorAsignatura() throws IOException {
+        BufferedReader buffR = new BufferedReader(new FileReader(notas));
+        BufferedWriter buffW = new BufferedWriter(new FileWriter(mediaPorAsignatura));
+        Scanner sc = new Scanner(buffR);
+        int[] sumatoriasParciales = new int[cantMaterias];
+        for (int i = 0; i < cantEstudiantes; i++) {
+
+            for (int j = 0; j < cantMaterias; j++) {
+                sumatoriasParciales[j] += sc.nextInt();
+                //System.out.println(sumatoriasParciales[j]);
+            }
+
+        }
+        int promedio = 0;
+        for (int x = 0; x < cantMaterias; x++) {
+            promedio = sumatoriasParciales[x] / cantEstudiantes;
+            buffW.write(promedio + "\n");
+
+        }
+        buffW.close();
+        buffR.close();
+    }
+
+    public static void mediaTotalClase() throws IOException{
+        BufferedReader buffR = new BufferedReader(new FileReader(mediaPorAlumno));
+        BufferedWriter buffW = new BufferedWriter(new FileWriter(mediaDeLaClase));
+        Scanner sc = new Scanner(buffR);
+        int promedio=0;
+        for(int i=0;i<cantEstudiantes;i++){
+            promedio+=sc.nextInt();
+            System.out.println(promedio);
+        }
+        double media=promedio/cantEstudiantes;
+        buffW.write(Double.toString(media));
+        
+        buffR.close();
+        buffW.close();
+    }
+
+    public static void cargarTxt() throws IOException {
+        BufferedWriter buff = new BufferedWriter(new FileWriter(notas));
+        int notaRandom;
+        for (int i = 0; i < cantEstudiantes; i++) {
+            for (int j = 0; j < cantMaterias; j++) {
+                notaRandom = (int) (1 + (Math.random() * (10 - 1)));
+                buff.write(Integer.toString(notaRandom) + " ");
+                if (j == (cantMaterias - 1)) {
+                    buff.write("\n");
+                }
+            }
+        }
+        buff.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        // cargarTxt();
+        //calcularMediaPorAlumno();
+        //calcularMediaPorAsignatura();
+        mediaTotalClase();
+    }
+}
